a2a0a3559b6a3c30190a524bc2650b43f099942e
elenimikro <mikrohelen@gmail.com>
2013-04-15 17:07:09 +0100

    Code for comparing clusters

---
+++ similarity/src/experiments/ClusterComparison.java
@@ -0,0 +1,40 @@
+package experiments;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLEntity;
+
+public class ClusterComparison<O> {
+
+	Comparator<Set<OWLEntity>> sizeComparator = new Comparator<Set<OWLEntity>>() {
+		@Override
+		public int compare(Set<OWLEntity> o1, Set<OWLEntity> o2) {
+			return o1.size() - o2.size();
+		}
+	};
+
+	public Set<O> getIntersection(Set<O> cluster, Set<O> anotherCluster) {
+		Set<O> intersection = new HashSet<O>();
+		intersection.addAll(cluster);
+		intersection.retainAll(anotherCluster);
+		return intersection;
+	}
+
+	public double getClusterSimilarity(Set<O> cluster, Set<O> anotherCluster) {
+		if (cluster.size() == 0 && anotherCluster.size() == 0) {
+			return 0;
+		} else {
+			Set<O> union = new HashSet<O>();
+			Set<O> intersection = new HashSet<O>();
+			union.addAll(cluster);
+			union.addAll(anotherCluster);
+			intersection.addAll(cluster);
+			intersection.retainAll(anotherCluster);
+
+			double toReturn = ((double) intersection.size()) / union.size();
+			return toReturn;
+		}
+	}
+}
