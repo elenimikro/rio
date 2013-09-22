598bb5248470d978785d70516fa8e0991791def7
elenimikro <mikrohelen@gmail.com>
2013-09-22 21:15:47 +0100

    -generation of lexical regularities with multiple variables.

---
+++ scrapbook/test/src/org/coode/lexical/pattern/LexicalPatternParserTest.java
@@ -0,0 +1,35 @@
+package org.coode.lexical.pattern;
+
+import java.io.File;
+
+import org.coode.lexical.pattern.clustering.LexicalPatternParser;
+import org.junit.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.util.MultiMap;
+
+public class LexicalPatternParserTest {
+
+	@Test
+	public void testXmlParser() {
+		try {
+			String inputFile = "/Users/elenimikroyannidi/LexicalAnalysis/SO/SequenceOnt_CaseSensitivefalse_PerctCov_Partition_5.0_ExtraRandom.xml";
+			File ontoFile = new File(
+					"/Users/elenimikroyannidi/LexicalAnalysis/so_2_5_1.obo");
+			OWLOntology o = OWLManager.createOWLOntologyManager()
+					.loadOntologyFromOntologyDocument(ontoFile);
+			LexicalPatternParser parser = new LexicalPatternParser(inputFile, o);
+			MultiMap<String, OWLEntity> patMap = parser.getLexicalPatternMap();
+			for (String pat : patMap.keySet()) {
+				System.out.println("Pattern: " + pat);
+				for (OWLEntity e : patMap.get(pat)) {
+					System.out.println("\t" + e);
+				}
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
